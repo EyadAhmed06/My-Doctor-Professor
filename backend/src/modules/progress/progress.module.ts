1601907084c15559018c4a75b55e0d1a1fd8f572
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 16:02:33 +0300

    Wire backend controllers into domain modules

--- backend/src/modules/progress/progress.module.ts
+++ backend/src/modules/progress/progress.module.ts
@@ -1,12 +1,2 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { StudentCourseProgress } from '../../common/entities/student-course-progress.entity';
-import { StudentLectureProgress } from '../../common/entities/student-lecture-progress.entity';
-import { StudentQuestionProgress } from '../../common/entities/student-question-progress.entity';
-import { StudentTopicProgress } from '../../common/entities/student-topic-progress.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([StudentCourseProgress, StudentLectureProgress, StudentTopicProgress, StudentQuestionProgress])],
-  exports: [TypeOrmModule],
-})
-export class ProgressModule {}
+import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm'; import { StudentCourseProgress } from '../../common/entities/student-course-progress.entity'; import { StudentLectureProgress } from '../../common/entities/student-lecture-progress.entity'; import { StudentQuestionProgress } from '../../common/entities/student-question-progress.entity'; import { StudentTopicProgress } from '../../common/entities/student-topic-progress.entity'; import { ProgressController } from './progress.controller';
+@Module({imports:[TypeOrmModule.forFeature([StudentCourseProgress,StudentLectureProgress,StudentTopicProgress,StudentQuestionProgress])],controllers:[ProgressController],exports:[TypeOrmModule]}) export class ProgressModule {}
