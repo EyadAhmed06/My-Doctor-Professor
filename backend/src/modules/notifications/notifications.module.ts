87841f6551352015dd7ae150e1339065e63fc960
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 18:00:45 +0300

    Wire notification service dependencies

--- backend/src/modules/notifications/notifications.module.ts
+++ backend/src/modules/notifications/notifications.module.ts
@@ -1,2 +1,15 @@
-import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm'; import { Notification } from '../../common/entities/notification.entity'; import { UserNotification } from '../../common/entities/user-notification.entity'; import { NotificationsController } from './notifications.controller';
-@Module({imports:[TypeOrmModule.forFeature([Notification,UserNotification])],controllers:[NotificationsController],exports:[TypeOrmModule]}) export class NotificationsModule {}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notification } from '../../common/entities/notification.entity';
+import { UserNotification } from '../../common/entities/user-notification.entity';
+import { User } from '../users/entities/user.entity';
+import { NotificationsController } from './notifications.controller';
+import { NotificationsService } from './notifications.service';
+
+@Module({
+ imports:[TypeOrmModule.forFeature([Notification,UserNotification,User])],
+ controllers:[NotificationsController],
+ providers:[NotificationsService],
+ exports:[NotificationsService,TypeOrmModule],
+})
+export class NotificationsModule {}
