80cce5453bdcebdc2d3bcd5ad26d672a68ebfc38
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 17:02:18 +0300

    Register question bank service

--- backend/src/modules/questions/questions.module.ts
+++ backend/src/modules/questions/questions.module.ts
@@ -1,2 +1,27 @@
-import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm'; import { EssayConfiguration } from '../../common/entities/essay-configuration.entity'; import { McqOption } from '../../common/entities/mcq-option.entity'; import { Question } from '../../common/entities/question.entity'; import { QuestionTag } from '../../common/entities/question-tag.entity'; import { Tag } from '../../common/entities/tag.entity'; import { QuestionsController } from './questions.controller';
-@Module({imports:[TypeOrmModule.forFeature([Question,McqOption,EssayConfiguration,Tag,QuestionTag])],controllers:[QuestionsController],exports:[TypeOrmModule]}) export class QuestionsModule {}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EssayConfiguration } from '../../common/entities/essay-configuration.entity';
+import { McqOption } from '../../common/entities/mcq-option.entity';
+import { Question } from '../../common/entities/question.entity';
+import { QuestionTag } from '../../common/entities/question-tag.entity';
+import { Tag } from '../../common/entities/tag.entity';
+import { Topic } from '../../common/entities/topic.entity';
+import { QuestionsController } from './questions.controller';
+import { QuestionsService } from './questions.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Question,
+      McqOption,
+      EssayConfiguration,
+      Tag,
+      QuestionTag,
+      Topic,
+    ]),
+  ],
+  controllers: [QuestionsController],
+  providers: [QuestionsService],
+  exports: [QuestionsService, TypeOrmModule],
+})
+export class QuestionsModule {}
