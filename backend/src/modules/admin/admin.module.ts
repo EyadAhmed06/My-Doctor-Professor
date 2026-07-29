fbeeaed17fbc1bc6174b7cf68ffb07bddc3af718
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 16:02:43 +0300

    Wire backend controllers into domain modules

--- backend/src/modules/admin/admin.module.ts
+++ backend/src/modules/admin/admin.module.ts
@@ -1,4 +1,1 @@
-import { Module } from '@nestjs/common';
-
-@Module({})
-export class AdminModule {}
+import { Module } from '@nestjs/common'; import { AdminController } from './admin.controller'; @Module({controllers:[AdminController]}) export class AdminModule {}
