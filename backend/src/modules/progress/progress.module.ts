a9611e583f355b5237b2636e89549aab5a4596e6
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 17:53:04 +0300

    Wire progress service dependencies

--- backend/src/modules/progress/progress.module.ts
+++ backend/src/modules/progress/progress.module.ts
@@ -1,2 +1,24 @@
-import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm'; import { StudentCourseProgress } from '../../common/entities/student-course-progress.entity'; import { StudentLectureProgress } from '../../common/entities/student-lecture-progress.entity'; import { StudentQuestionProgress } from '../../common/entities/student-question-progress.entity'; import { StudentTopicProgress } from '../../common/entities/student-topic-progress.entity'; import { ProgressController } from './progress.controller';
-@Module({imports:[TypeOrmModule.forFeature([StudentCourseProgress,StudentLectureProgress,StudentTopicProgress,StudentQuestionProgress])],controllers:[ProgressController],exports:[TypeOrmModule]}) export class ProgressModule {}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from '../../common/entities/course.entity';
+import { Lecture } from '../../common/entities/lecture.entity';
+import { Question } from '../../common/entities/question.entity';
+import { StudentCourseProgress } from '../../common/entities/student-course-progress.entity';
+import { StudentLectureProgress } from '../../common/entities/student-lecture-progress.entity';
+import { StudentQuestionProgress } from '../../common/entities/student-question-progress.entity';
+import { StudentTopicProgress } from '../../common/entities/student-topic-progress.entity';
+import { Topic } from '../../common/entities/topic.entity';
+import { Student } from '../users/entities/student.entity';
+import { ProgressController } from './progress.controller';
+import { ProgressService } from './progress.service';
+
+@Module({
+ imports:[TypeOrmModule.forFeature([
+  StudentCourseProgress,StudentLectureProgress,StudentTopicProgress,
+  StudentQuestionProgress,Course,Lecture,Topic,Question,Student,
+ ])],
+ controllers:[ProgressController],
+ providers:[ProgressService],
+ exports:[ProgressService,TypeOrmModule],
+})
+export class ProgressModule {}
