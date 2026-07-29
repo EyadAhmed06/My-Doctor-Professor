6b6ef68fb33a3ef4883fd7615a2cd4ad216666be
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 16:02:34 +0300

    Wire backend controllers into domain modules

--- backend/src/modules/notifications/notifications.module.ts
+++ backend/src/modules/notifications/notifications.module.ts
@@ -1,10 +1,2 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Notification } from '../../common/entities/notification.entity';
-import { UserNotification } from '../../common/entities/user-notification.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Notification, UserNotification])],
-  exports: [TypeOrmModule],
-})
-export class NotificationsModule {}
+import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm'; import { Notification } from '../../common/entities/notification.entity'; import { UserNotification } from '../../common/entities/user-notification.entity'; import { NotificationsController } from './notifications.controller';
+@Module({imports:[TypeOrmModule.forFeature([Notification,UserNotification])],controllers:[NotificationsController],exports:[TypeOrmModule]}) export class NotificationsModule {}
