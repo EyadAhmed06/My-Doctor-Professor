42048581c7b6be56aacb0d11a3f7e18cdf0500cf
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 16:02:28 +0300

    Wire backend controllers into domain modules

--- backend/src/modules/academic/academic.module.ts
+++ backend/src/modules/academic/academic.module.ts
@@ -1,14 +1,2 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Course } from '../../common/entities/course.entity';
-import { Lecture } from '../../common/entities/lecture.entity';
-import { Resource } from '../../common/entities/resource.entity';
-import { Semester } from '../../common/entities/semester.entity';
-import { Topic } from '../../common/entities/topic.entity';
-import { Week } from '../../common/entities/week.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Semester, Course, Week, Lecture, Topic, Resource])],
-  exports: [TypeOrmModule],
-})
-export class AcademicModule {}
+import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm'; import { Course } from '../../common/entities/course.entity'; import { Lecture } from '../../common/entities/lecture.entity'; import { Resource } from '../../common/entities/resource.entity'; import { Semester } from '../../common/entities/semester.entity'; import { Topic } from '../../common/entities/topic.entity'; import { Week } from '../../common/entities/week.entity'; import { AcademicController } from './academic.controller';
+@Module({imports:[TypeOrmModule.forFeature([Semester,Course,Week,Lecture,Topic,Resource])],controllers:[AcademicController],exports:[TypeOrmModule]}) export class AcademicModule {}
