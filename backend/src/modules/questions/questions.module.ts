e1e23f55c8fa9c703eb5993732c2148d302ea1a4
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 16:02:30 +0300

    Wire backend controllers into domain modules

--- backend/src/modules/questions/questions.module.ts
+++ backend/src/modules/questions/questions.module.ts
@@ -1,13 +1,2 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { EssayConfiguration } from '../../common/entities/essay-configuration.entity';
-import { McqOption } from '../../common/entities/mcq-option.entity';
-import { Question } from '../../common/entities/question.entity';
-import { QuestionTag } from '../../common/entities/question-tag.entity';
-import { Tag } from '../../common/entities/tag.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Question, McqOption, EssayConfiguration, Tag, QuestionTag])],
-  exports: [TypeOrmModule],
-})
-export class QuestionsModule {}
+import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm'; import { EssayConfiguration } from '../../common/entities/essay-configuration.entity'; import { McqOption } from '../../common/entities/mcq-option.entity'; import { Question } from '../../common/entities/question.entity'; import { QuestionTag } from '../../common/entities/question-tag.entity'; import { Tag } from '../../common/entities/tag.entity'; import { QuestionsController } from './questions.controller';
+@Module({imports:[TypeOrmModule.forFeature([Question,McqOption,EssayConfiguration,Tag,QuestionTag])],controllers:[QuestionsController],exports:[TypeOrmModule]}) export class QuestionsModule {}
