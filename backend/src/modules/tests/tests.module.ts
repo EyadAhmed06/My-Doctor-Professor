1504939739b4b33188a7f165f837fcd8572aa09d
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 16:02:26 +0300

    Wire backend controllers into domain modules

--- backend/src/modules/tests/tests.module.ts
+++ backend/src/modules/tests/tests.module.ts
@@ -1,14 +1,2 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { QuestionFlag } from '../../common/entities/question-flag.entity';
-import { QuestionNote } from '../../common/entities/question-note.entity';
-import { StudentAnswer } from '../../common/entities/student-answer.entity';
-import { TestAttempt } from '../../common/entities/test-attempt.entity';
-import { TestQuestion } from '../../common/entities/test-question.entity';
-import { Test } from '../../common/entities/test.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Test, TestQuestion, TestAttempt, StudentAnswer, QuestionFlag, QuestionNote])],
-  exports: [TypeOrmModule],
-})
-export class TestsModule {}
+import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm'; import { QuestionFlag } from '../../common/entities/question-flag.entity'; import { QuestionNote } from '../../common/entities/question-note.entity'; import { StudentAnswer } from '../../common/entities/student-answer.entity'; import { TestAttempt } from '../../common/entities/test-attempt.entity'; import { TestQuestion } from '../../common/entities/test-question.entity'; import { Test } from '../../common/entities/test.entity'; import { TestsController } from './tests.controller';
+@Module({imports:[TypeOrmModule.forFeature([Test,TestQuestion,TestAttempt,StudentAnswer,QuestionFlag,QuestionNote])],controllers:[TestsController],exports:[TypeOrmModule]}) export class TestsModule {}
