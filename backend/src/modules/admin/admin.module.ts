e7b7a694ffacb982db6ca07be0671b5ae835b70b
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 18:17:21 +0300

    Wire administration service dependencies

--- backend/src/modules/admin/admin.module.ts
+++ backend/src/modules/admin/admin.module.ts
@@ -1,1 +1,21 @@
-import { Module } from '@nestjs/common'; import { AdminController } from './admin.controller'; @Module({controllers:[AdminController]}) export class AdminModule {}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { Instructor } from '../users/entities/instructor.entity';
+import { Student } from '../users/entities/student.entity';
+import { SystemAdmin } from '../users/entities/system-admin.entity';
+import { User } from '../users/entities/user.entity';
+import { UsersModule } from '../users/users.module';
+import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
+
+@Module({
+ imports:[
+  TypeOrmModule.forFeature([User,Student,Instructor,SystemAdmin]),
+  UsersModule,AuthModule,
+ ],
+ controllers:[AdminController],
+ providers:[AdminService],
+ exports:[AdminService],
+})
+export class AdminModule {}
