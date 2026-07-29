3becd100614837fe55fce064f9796834cc0e1203
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 16:14:25 +0300

    Register secure authentication sessions

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -1,3 +1,18 @@
-import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm';
-import { InstructorAvailability } from './entities/instructor-availability.entity'; import { Instructor } from './entities/instructor.entity'; import { Student } from './entities/student.entity'; import { SystemAdmin } from './entities/system-admin.entity'; import { User } from './entities/user.entity'; import { UsersController } from './users.controller'; import { UsersService } from './users.service';
-@Module({imports:[TypeOrmModule.forFeature([User,Student,Instructor,SystemAdmin,InstructorAvailability])],controllers:[UsersController],providers:[UsersService],exports:[UsersService]}) export class UsersModule {}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthSession } from './entities/auth-session.entity';
+import { InstructorAvailability } from './entities/instructor-availability.entity';
+import { Instructor } from './entities/instructor.entity';
+import { Student } from './entities/student.entity';
+import { SystemAdmin } from './entities/system-admin.entity';
+import { User } from './entities/user.entity';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Student, Instructor, SystemAdmin, InstructorAvailability, AuthSession])],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService],
+})
+export class UsersModule {}
