3b6021c7acecffb0a3874e80f47d1717cd2191d1
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 16:02:36 +0300

    Wire backend controllers into domain modules

--- backend/src/modules/audit/audit.module.ts
+++ backend/src/modules/audit/audit.module.ts
@@ -1,9 +1,2 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuditLog } from '../../common/entities/audit-log.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([AuditLog])],
-  exports: [TypeOrmModule],
-})
-export class AuditModule {}
+import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm'; import { AuditLog } from '../../common/entities/audit-log.entity'; import { AuditController } from './audit.controller';
+@Module({imports:[TypeOrmModule.forFeature([AuditLog])],controllers:[AuditController],exports:[TypeOrmModule]}) export class AuditModule {}
