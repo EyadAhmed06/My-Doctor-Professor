53d0752eca66cc7fea4badf486c9434429a975c8
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 17:34:55 +0300

    Wire flashcard DTOs controllers and service

--- backend/src/modules/flashcards/flashcards.module.ts
+++ backend/src/modules/flashcards/flashcards.module.ts
@@ -1,2 +1,21 @@
-import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm'; import { FlashcardDeck } from '../../common/entities/flashcard-deck.entity'; import { Flashcard } from '../../common/entities/flashcard.entity'; import { StudentFlashcardProgress } from '../../common/entities/student-flashcard-progress.entity'; import { FlashcardsController } from './flashcards.controller';
-@Module({imports:[TypeOrmModule.forFeature([FlashcardDeck,Flashcard,StudentFlashcardProgress])],controllers:[FlashcardsController],exports:[TypeOrmModule]}) export class FlashcardsModule {}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from '../../common/entities/course.entity';
+import { FlashcardDeck } from '../../common/entities/flashcard-deck.entity';
+import { Flashcard } from '../../common/entities/flashcard.entity';
+import { Lecture } from '../../common/entities/lecture.entity';
+import { StudentFlashcardProgress } from '../../common/entities/student-flashcard-progress.entity';
+import { Topic } from '../../common/entities/topic.entity';
+import { Student } from '../users/entities/student.entity';
+import { FlashcardsController } from './flashcards.controller';
+import { FlashcardsService } from './flashcards.service';
+
+@Module({
+ imports:[TypeOrmModule.forFeature([
+  FlashcardDeck,Flashcard,StudentFlashcardProgress,Course,Lecture,Topic,Student,
+ ])],
+ controllers:[FlashcardsController],
+ providers:[FlashcardsService],
+ exports:[FlashcardsService,TypeOrmModule],
+})
+export class FlashcardsModule {}
