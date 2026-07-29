b35eee8742bb0d80a15b9dff5de0f2aedf7864e1
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-29 16:02:23 +0300

    Wire backend controllers into domain modules

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -1,24 +1,3 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { Student } from './entities/student.entity';
-import { Instructor } from './entities/instructor.entity';
-import { SystemAdmin } from './entities/system-admin.entity';
-import { InstructorAvailability } from './entities/instructor-availability.entity';
-import { UsersService } from './users.service';
-
-@Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      User,
-      Student,
-      Instructor,
-      SystemAdmin,
-      InstructorAvailability,
-    ]),
-  ],
-  providers: [UsersService],
-  exports: [UsersService],
-})
-export class UsersModule {}
-
+import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm';
+import { InstructorAvailability } from './entities/instructor-availability.entity'; import { Instructor } from './entities/instructor.entity'; import { Student } from './entities/student.entity'; import { SystemAdmin } from './entities/system-admin.entity'; import { User } from './entities/user.entity'; import { UsersController } from './users.controller'; import { UsersService } from './users.service';
+@Module({imports:[TypeOrmModule.forFeature([User,Student,Instructor,SystemAdmin,InstructorAvailability])],controllers:[UsersController],providers:[UsersService],exports:[UsersService]}) export class UsersModule {}
