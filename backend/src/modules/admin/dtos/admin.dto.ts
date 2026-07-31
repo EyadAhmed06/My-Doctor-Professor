407523661103a9a9e599ac3099a945fa75a2d43b
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-07-31 17:31:59 +0300

    Add secure account bootstrap workflow

--- backend/src/modules/admin/dtos/admin.dto.ts
+++ backend/src/modules/admin/dtos/admin.dto.ts
@@ -45,3 +45,38 @@ export class ImportUsersDto {
 export class StatisticsQueryDto {
  @IsOptional() @IsEnum(UserRole) role?:UserRole;
 }
+
+export class BootstrapAdminDto {
+ @IsString() @IsNotEmpty() @MaxLength(150) full_name:string;
+ @IsEmail() @MaxLength(320) email:string;
+ @IsString() @MinLength(12) @MaxLength(128)
+ @Matches(/[a-z]/) @Matches(/[A-Z]/) @Matches(/[0-9]/) password:string;
+ @IsPhoneNumber() phone_number:string;
+ @IsString() @IsNotEmpty() @MaxLength(30) employee_number:string;
+}
+
+export class BootstrapInstructorDto {
+ @IsString() @IsNotEmpty() @MaxLength(150) full_name:string;
+ @IsEmail() @MaxLength(320) email:string;
+ @IsString() @MinLength(12) @MaxLength(128)
+ @Matches(/[a-z]/) @Matches(/[A-Z]/) @Matches(/[0-9]/) password:string;
+ @IsPhoneNumber() phone_number:string;
+ @IsOptional() @IsString() @MaxLength(150) specialization?:string;
+ @IsOptional() @IsString() @MaxLength(100) office_location?:string;
+}
+
+export class BootstrapStudentDto {
+ @IsString() @IsNotEmpty() @MaxLength(150) full_name:string;
+ @IsEmail() @MaxLength(320) email:string;
+ @IsString() @MinLength(12) @MaxLength(128)
+ @Matches(/[a-z]/) @Matches(/[A-Z]/) @Matches(/[0-9]/) password:string;
+ @IsPhoneNumber() phone_number:string;
+ @IsString() @IsNotEmpty() @MaxLength(30) student_number:string;
+ @IsInt() @Min(1) current_semester:number;
+}
+
+export class BootstrapAccountsDto {
+ @ValidateNested() @Type(()=>BootstrapAdminDto) admin:BootstrapAdminDto;
+ @ValidateNested() @Type(()=>BootstrapInstructorDto) instructor:BootstrapInstructorDto;
+ @ValidateNested() @Type(()=>BootstrapStudentDto) student:BootstrapStudentDto;
+}
