df4769bce1f96a80ed3fd79a709b2df226225fe4
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-08-10 18:54:22 +0300

    Register assessment launch configuration controller

--- backend/src/modules/tests/tests.module.ts
+++ backend/src/modules/tests/tests.module.ts
@@ -14,6 +14,7 @@ import { Week } from '../../common/entities/week.entity';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { Student } from '../users/entities/student.entity';
 import { AssessmentAuthoringService } from './assessment-authoring.service';
+import { TestLaunchController } from './test-launch.controller';
 import { TestsController } from './tests.controller';
 import { TestsService } from './tests.service';
 
@@ -22,7 +23,7 @@ import { TestsService } from './tests.service';
   Test,TestQuestion,TestAttempt,StudentAnswer,QuestionFlag,QuestionNote,
   Question,McqOption,Course,Week,Lecture,Student,
  ])],
- controllers:[TestsController],
+ controllers:[TestsController,TestLaunchController],
  providers:[TestsService,AssessmentAuthoringService],
  exports:[TestsService,TypeOrmModule],
 })
