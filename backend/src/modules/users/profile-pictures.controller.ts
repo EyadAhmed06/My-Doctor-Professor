b14a7828ef385399c8d316f732bbaae1c9a466c9
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-08-28 21:39:00 +0300

    fix: serve persisted profile pictures publicly

--- backend/src/modules/users/profile-pictures.controller.ts
+++ backend/src/modules/users/profile-pictures.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Header, Param, StreamableFile } from '@nestjs/common';
+import { Public } from '../auth/decorators/public.decorator';
 import { ProfilePictureStorageService } from './profile-picture-storage.service';
 
 @Controller('profile-pictures')
 export class ProfilePicturesController {
   constructor(private readonly storage: ProfilePictureStorageService) {}
 
+  @Public()
   @Get(':key')
   @Header('Cache-Control', 'public, max-age=86400')
   @Header('X-Content-Type-Options', 'nosniff')
