d832d96e8a00dc3f431cc5f147cb688e0521dfa2
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-08-28 17:18:12 +0300

    phase5: register achievement feedback service

--- backend/src/modules/progress/progress.module.ts
+++ backend/src/modules/progress/progress.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from '../../common/entities/course.entity';
 import { Lecture } from '../../common/entities/lecture.entity';
 import { Question } from '../../common/entities/question.entity';
+import { StudentAchievement } from '../../common/entities/student-achievement.entity';
 import { StudentCourseProgress } from '../../common/entities/student-course-progress.entity';
 import { StudentLectureProgress } from '../../common/entities/student-lecture-progress.entity';
 import { StudentQuestionProgress } from '../../common/entities/student-question-progress.entity';
@@ -10,16 +11,17 @@ import { StudentTopicProgress } from '../../common/entities/student-topic-progre
 import { Topic } from '../../common/entities/topic.entity';
 import { AcademicModule } from '../academic/academic.module';
 import { Student } from '../users/entities/student.entity';
+import { AchievementFeedbackService } from './achievement-feedback.service';
 import { ProgressController } from './progress.controller';
 import { ProgressService } from './progress.service';
 
 @Module({
  imports:[AcademicModule,TypeOrmModule.forFeature([
   StudentCourseProgress,StudentLectureProgress,StudentTopicProgress,
-  StudentQuestionProgress,Course,Lecture,Topic,Question,Student,
+  StudentQuestionProgress,StudentAchievement,Course,Lecture,Topic,Question,Student,
  ])],
  controllers:[ProgressController],
- providers:[ProgressService],
- exports:[ProgressService,TypeOrmModule],
+ providers:[ProgressService,AchievementFeedbackService],
+ exports:[ProgressService,AchievementFeedbackService,TypeOrmModule],
 })
 export class ProgressModule {}
