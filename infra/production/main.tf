9afcec0a87f1ba3b355a9afac64911c20bea3a36
Eyad Ahmed <eyadelmaleh07@gmail.com>
2026-09-06 18:34:17 +0300

    infra: move production subnet and database volume to supported AZ

--- infra/production/main.tf
+++ infra/production/main.tf
@@ -35,7 +35,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.24.1.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[0]
+  availability_zone       = var.app_availability_zone
   map_public_ip_on_launch = true
   tags                    = { Name = "${local.name}-public-a" }
 }
